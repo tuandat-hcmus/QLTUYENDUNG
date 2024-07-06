7a3b8996539d820ed5bc62081bee1aa6417909fb
21120442 <duydangtr@gmail.com>
2024-07-06 18:50:22 +0700

    fix design

--- QLTUYENDUNG/NVPhongphaply/PhongPhapLyHome.Designer.cs
+++ QLTUYENDUNG/NVPhongphaply/PhongPhapLyHome.Designer.cs
@@ -30,11 +30,15 @@
         {
             this.btnThongKe = new System.Windows.Forms.Button();
             this.btnGiaHan = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnThongKe
             // 
-            this.btnThongKe.Location = new System.Drawing.Point(199, 155);
+            this.btnThongKe.Location = new System.Drawing.Point(151, 64);
             this.btnThongKe.Name = "btnThongKe";
             this.btnThongKe.Size = new System.Drawing.Size(75, 23);
             this.btnThongKe.TabIndex = 0;
@@ -44,7 +48,7 @@
             // 
             // btnGiaHan
             // 
-            this.btnGiaHan.Location = new System.Drawing.Point(214, 213);
+            this.btnGiaHan.Location = new System.Drawing.Point(166, 122);
             this.btnGiaHan.Name = "btnGiaHan";
             this.btnGiaHan.Size = new System.Drawing.Size(75, 23);
             this.btnGiaHan.TabIndex = 1;
@@ -52,20 +56,49 @@
             this.btnGiaHan.UseVisualStyleBackColor = true;
             this.btnGiaHan.Click += new System.EventHandler(this.btnGiaHan_Click);
             // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 76);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(201, 485);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnGiaHan);
+            this.panel4.Controls.Add(this.btnThongKe);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(201, 76);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(783, 485);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(86)))), ((int)(((byte)(182)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(984, 76);
+            this.panel2.TabIndex = 3;
+            // 
             // PhongPhapLyHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(700, 422);
-            this.Controls.Add(this.btnGiaHan);
-            this.Controls.Add(this.btnThongKe);
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "PhongPhapLyHome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -74,5 +107,8 @@
 
         private System.Windows.Forms.Button btnThongKe;
         private System.Windows.Forms.Button btnGiaHan;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
     }
 }
