b8eb11ccebc25e19139ff445f2a5b7f4073fca68
DuongKhanhAn <119850695+DuongKhanhAn@users.noreply.github.com>
2024-07-08 10:02:58 +0700

    Add files via upload

---
+++ QLTUYENDUNG/DTO/DoanhNghiepDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVTD1.DTO
+{
+    public class DoanhNghiepDTO
+    {
+        public string IDDoanhNghiep { get; set; } = string.Empty;
+        public string Ten { get; set; } = string.Empty;
+    }
+}
+
---
+++ QLTUYENDUNG/DTO/UngVienDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace NVTD1.DTO
+{
+    public class UngVienDTO
+    {
+        public string IDUngVien { get; set; } = string.Empty;
+        public string Ten { get; set; } = string.Empty;
+        public string GioiTinh { get; set; } = string.Empty;
+        public string IDHoSo { get; set; } = string.Empty;
+        public string HocVan { get; set; } = string.Empty;
+        public string TenBangCap { get; set; } = string.Empty;
+        public string KinhNghiem { get; set; } = string.Empty;
+        public string KiNang { get; set; } = string.Empty;
+        public string ViTri { get; set; } = string.Empty;
+        public string ThoiGianDT { get; set; } = string.Empty;
+        public string NgayHetHan { get; set; } = string.Empty;
+        public string TinhTrang { get; set; } = string.Empty;
+    }
+}
+
